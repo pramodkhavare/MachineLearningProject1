ca443e8fc05d210982fbec68470fe7d55f9aea2d
pramodkhavare <pramodkhavare2000@gmail.com>
2024-03-02 15:19:31 +0530

    Fourth Commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/" ,methods= ['GET' , 'POST'])
 def index():
-    return "Starting Machine Learning Project"
+    return "CICD Pipeline has been estabilished"
 
 
 
